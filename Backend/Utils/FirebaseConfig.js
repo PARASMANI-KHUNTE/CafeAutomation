9d5eaea107e3724ce940332e7a9dfcf0e683b619
PARASMANI-KHUNTE <parasmanikhunte@gmail.com>
2025-05-06 16:36:50 +0530

    fixed the no module issue

--- Backend/Utils/FirebaseConfig.js
+++ Backend/Utils/FirebaseConfig.js
@@ -5,7 +5,8 @@ const path = require('path');
 
 
 // Load the service account from the secret file path
-const serviceAccount = require(path.join(__dirname, `/etc/secrets/${process.env.SERVICE_ACCOUNT}`));
+// const serviceAccount = require(path.join(__dirname, `/etc/secrets/${process.env.SERVICE_ACCOUNT}`));
+const serviceAccount =`/etc/secrets/${process.env.SERVICE_ACCOUNT}`;
 
 if (!process.env.storageBucket) {
     console.error('Firebase storage bucket not configured. Please set storageBucket in .env file');
